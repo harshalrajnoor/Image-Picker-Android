63edd97cd2d80dbb8ed3d8b8d321294b3bdd59b1
Harshal Rajnoor <harshalrajnor852056@gmail.com>
2023-11-09 08:45:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/image_picker_lib/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.image_picker_lib;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(MainActivity.this)
+                        //.crop()	    			//Crop image(Optional), Check Customization for more option
+                        //.compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        //.maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri uri = data.getData();
+        imageView.setImageURI(uri);
+    }
+}
